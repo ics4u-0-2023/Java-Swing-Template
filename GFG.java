4713969daaa2b705d63d51a58b4c709ef6ffad35
ICS4U-0 2023 <134095878+ics4u-0-2023@users.noreply.github.com>
2024-04-08 01:59:19 +0000

    update

---
+++ GFG.java
@@ -0,0 +1,33 @@
+// Java program using label (swing)
+// to display the message “GFG WEB Site Click”
+import java.io.*;
+import javax.swing.*;
+
+// Main class
+class GFG {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+		// Creating instance of JFrame
+		JFrame frame = new JFrame();
+
+		// Creating instance of JButton
+		JButton button = new JButton(" GFG WebSite Click");
+
+		// x axis, y axis, width, height
+		button.setBounds(50, 50, 220, 50);
+
+		// adding button in JFrame
+		frame.add(button);
+
+		// 400 width and 500 height
+		frame.setSize(400, 300);
+
+		// using no layout managers
+		frame.setLayout(null);
+
+		// making the frame visible
+		frame.setVisible(true);
+	}
+}
